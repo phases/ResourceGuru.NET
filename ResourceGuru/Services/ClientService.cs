4cf6e4dad3bb63d4e25af092f9fb5a7b41b4930f
Ajmal VH <avh@phases.dk>
2014-07-07 16:45:46 +0530

    added get client method

---
+++ ResourceGuru/Services/ClientService.cs
@@ -0,0 +1,36 @@
+﻿using ResourceGuru.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceGuru.Services
+{
+    public class ClientService
+    {
+        protected ResourceGuruClient _client;
+        public ClientService(ResourceGuruClient client)
+        {
+            _client = client;
+        }
+
+        public List<Client> GetClients(string subdomain, int? limit, int? offset)
+        {
+            string url = string.Format("/v1/{0}/clients", subdomain);
+            return _client.requestHelper.Get<List<Client>>(url);
+        }
+
+        public List<Client> GetArchivedClients(string subdomain, int? limit, int? offset)
+        {
+            string url = string.Format("/v1/{0}/clients/archived", subdomain);
+            return _client.requestHelper.Get<List<Client>>(url);
+        }
+
+        public ClientDetail GetClient(string subdomain, int clientId)
+        {
+            string url = string.Format("/v1/{0}/clients/{1}", subdomain, clientId);
+            return _client.requestHelper.Get<ClientDetail>(url);
+        }
+    }
+}
